b7e9da3f3a35d0f1a0adc0d52428200a5bcedb03
Tim Shaker <timshaker@gmail.com>
2018-06-29 21:58:16 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,24 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element){
+  var newArray = array.slice()
+  newArray.unshift(element)
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element){
+  var newArray = array.slice()
+  newArray.push(element)
+  return newArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+
